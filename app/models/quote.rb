5fc6645e3c37c14d4b8a6c15390e01f5a9d4010f
shybovycha <shybovycha@gmail.com>
2011-01-19 23:55:33 +0200

    Minor changes

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,2 +1,9 @@
 class Quote < ActiveRecord::Base
+	validates :author, 
+		:presence => true, 
+		:length => { :minimum => 3, :maximum => 80 }
+	validates :body, 
+		:presence => true, 
+		:length => { :minimum => 3, :maximum => 4096 },
+		:uniqueness => true
 end
