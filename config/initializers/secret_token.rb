f2ff5f7f6ba9ae9e6d9ebfe8402737ff55aec120
shybovycha <shybovycha@gmail.com>
2011-01-19 08:53:22 +0200

    Initial point

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StrongSword::Application.config.secret_token = '4e4a0790d02b0bc6090172a824cbad44470073bbd574a1ae10168f28685d50e99c93cb2cd13f16b87ea550fac8c34e42be57f0fa3ae89407b7298766920fe67e'
