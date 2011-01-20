c31e5c59077237c71375923c6d2429dc621c8382
shybovycha <shybovycha@gmail.com>
2011-01-20 19:36:01 +0200

    route fix

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,8 @@ StrongSword::Application.routes.draw do
 
   root :to => "quotes#index"
 
+  match "quotes/:author" => "quotes#author", :author => /[\w\s\D]{3,}/u
   match "quotes/:id" => "quotes#show", :id => /[\d]{1,}/
-  match "quotes/:author" => "quotes#author", :author => /[^\d]{3,}/u
   match "help/" => "statics#help"
   match "help/about" => "statics#about"
 
