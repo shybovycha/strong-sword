8c8131810507bba2390900817cb287ab510134c4
shybovycha <shybovycha@gmail.com>
2011-01-20 20:52:31 +0200

    Route becomes less-suck or not?

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ StrongSword::Application.routes.draw do
 
   #match 'author/:author' => "quotes#author"
   #match 'quotes/:id' => "quotes#show"
-  match 'quotes/:id' => 'quotes#show', :id => /\d{1,}/
+  #match 'quotes/:id' => 'quotes#show', :id => /\d{1,}/
   match 'quotes/:author' => 'quotes#author', :author => /[\w\s\D]{3,}/ui
   match 'help/' => "statics#help"
   match 'help/about' => "statics#about"
