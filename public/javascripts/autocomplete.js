be94c6437fbd8c775c25f17330a4b012252cb074
shybovycha <shybovycha@gmail.com>
2011-01-21 16:29:52 +0200

    Autocomplete fix

--- public/javascripts/autocomplete.js
+++ public/javascripts/autocomplete.js
@@ -1,9 +1,3 @@
 $(function() {
-	var list = [];
-	$.getJSON("/author_list", function(data) { list = data; });
-	
-	$("#quote_author").autocomplete({
-			source: list,
-			minLength: 1
-		});
+	$.getJSON("/author_list", function(data) { $("#quote_author").autocomplete({ source: data, minLength: 1 }) });
 });
