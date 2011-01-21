d1c849ae045b3422fdf7dc41c727b17cbf108d01
shybovycha <shybovycha@gmail.com>
2011-01-21 15:58:17 +0200

    JSON response fix try #2

--- public/javascripts/autocomplete.js
+++ public/javascripts/autocomplete.js
@@ -1,8 +1,9 @@
 $(function() {
 	$("#quote_author").autocomplete({
-			source: "/authors",
+			source: "/author_list",
 			minLength: 1
 		});
 		
-	alert("Hello, world!");
+	$.getJSON("/author_list", function(data) { alert(data); });
+	//alert("Hello, world!");
 });
