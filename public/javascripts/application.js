8cab39373e46c7c86488dccc96c7017691c837a1
shybovycha <shybovycha@gmail.com>
2011-01-22 11:44:10 +0200

    JS fix

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,31 +1,33 @@
 $(document).ready(function() {
 	$("[data-confirm]").click(function() { 
 		if (!confirm($(this).attr("data-confirm")))
-			return false; else
-				$(this).click();
+			return false;
+
+		if ($(this).attr("data-method")) {
+			var method = $(this).attr("data-method"),
+					url = $(this).attr("hhref"),
+				csrf_param = $('meta[name=csrf-param]')[0],
+				csrf_token = $('meta[name=csrf-token]')[0],
+				data = {};
+
+			if (method !== 'post')
+				data['_method'] = method;
+
+			if (csrf_param) {
+				var param = csrf_param.attr('content'),
+					token = csrf_token.attr('content');
+
+				data[param] = token;
+			}
+
+			if (!confirm(data))
+				return false; else
+					$.post(url, data, function(data) { alert(data); } );
+		}
 	});
 
 	$("a[data-method]").click(function() {
-		var method = $(this).attr("data-method"),
-			url = $(this).attr("hhref"),
-			csrf_param = $('meta[name=csrf-param]')[0],
-			csrf_token = $('meta[name=csrf-token]')[0],
-			data = {};
-
-		if (method !== 'post')
-			data['_method'] = method;
-
-		if (csrf_param) {
-			var param = csrf_param.attr('content'),
-				token = csrf_token.attr('content');
-
-			data[param] = token;
-		}
-
-		if (!confirm(data))
-			return false; else
-				$.post(url, data, function(data) { alert(data); } );
-		/*var form = new Element('form', { method: "POST", action: url, style: "display: none;" });
+			/*var form = new Element('form', { method: "POST", action: url, style: "display: none;" });
 		    $(this).parent().append(form);
 
 	    if (method !== 'post') {
