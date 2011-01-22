d387f334bcef1c34c24b4f6ef7d5309e74aaaf00
shybovycha <shybovycha@gmail.com>
2011-01-23 01:42:01 +0200

    a few translation fixes

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -12,7 +12,7 @@ class QuotesController < ApplicationController
     @quote = Quote.new
     #@quotes = Quote.find_by_approved(true)
 
-    @nav_menu = { :title => :latest_quotes, 
+    @nav_menu = { :title => t(:latest_quotes), 
     		      :links => [ #{ :title => "Add", :to => new_quote_path }, 
 		  	      { :title => :about, :to => "/help/about" },
 			      { :title => :help, :to => "/help/" }
@@ -83,7 +83,7 @@ class QuotesController < ApplicationController
 
     @quote = Quote.new
 
-    @nav_menu = { :title => :add_quote, 
+    @nav_menu = { :title => t(:add_quote), 
     		  :links => [ { :title => :help, :to => "/help/" },
     		              { :title => :back, :to => root_path } ] }
 
@@ -100,7 +100,7 @@ class QuotesController < ApplicationController
 
     @quote = Quote.find(params[:id])
 
-    @nav_menu = { :title => :edit, 
+    @nav_menu = { t(:title => :edit), 
     		  :links => [ { :title => :help, :to => "/help/" },
     		        { :title => :back, :to => root_path } ] }
   end
