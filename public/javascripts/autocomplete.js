9656336b2b12119bc4298d9c1e7b6077ded6d0bd
shybovycha <shybovycha@gmail.com>
2011-01-22 15:47:28 +0200

    JS fix

--- public/javascripts/autocomplete.js
+++ public/javascripts/autocomplete.js
@@ -1,3 +1,7 @@
+onerror = function moo(msg, url, line) {
+	alert('Error: ' + msg + '\nURL: ' + url + '\nLine: ' + line);
+}
+
 $(document).ready(function() {
 	$.getJSON("/author_list", function(data) { $("#quote_author").autocomplete({ source: data, minLength: 1 }) });
 });
