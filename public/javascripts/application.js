68e0d8c495aa42ec2d057634da0fb9ea79ee4740
shybovycha <shybovycha@gmail.com>
2011-01-22 11:14:34 +0200

    JS fix

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	$("[data-confirm]").click(function() { 
-		if (!confirm($(this).attr"href"))
+		if (!confirm($(this).attr("data-confirm")))
 			return false;
 	});
 });
