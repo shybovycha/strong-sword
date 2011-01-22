e4023086ea6e65383acf30cd1950fa7ebd0e977e
shybovycha <shybovycha@gmail.com>
2011-01-22 11:25:27 +0200

    JS fix

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -3,4 +3,28 @@ $(document).ready(function() {
 		if (!confirm($(this).attr("data-confirm")))
 			return false;
 	});
+
+	$("a[data-method]").click(function() {
+		var method = $(this).attr("data-method"),
+			url = $(this).attr("hhref"),
+			csrf_param = $('meta[name=csrf-param]')[0],
+			csrf_token = $('meta[name=csrf-token]')[0];
+
+		var form = new Element('form', { method: "POST", action: url, style: "display: none;" });
+		    element.parent().append(form);
+
+	    if (method !== 'post') {
+	      var field = new Element('input', { type: 'hidden', name: '_method', value: method });
+		      form.append(field);
+	    }
+
+	    if (csrf_param) {
+	      var param = csrf_param.attr('content'),
+		  token = csrf_token.attr('content'),
+		  field = new Element('input', { type: 'hidden', name: param, value: token });
+	      form.append(field);
+	    }
+
+	    form.submit();
+	});
 });
