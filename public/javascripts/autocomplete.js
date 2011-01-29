fc19c6da40cebd544fa91a9131820028a75d2ab2
shybovycha <shybovycha@gmail.com>
2011-01-29 08:57:37 +0200

    AJAX invoke try #1. Some fixes

--- public/javascripts/autocomplete.js
+++ public/javascripts/autocomplete.js
@@ -5,7 +5,9 @@ onerror = function moo(msg, url, line) {
 $(document).ready(function() {
 	$.getJSON("/author_list", function(data) { $("#quote_author").autocomplete({ source: data, minLength: 1 }) });
 	$("form.new_quote > .actions > [type=submit]").live("click", function() {
-		$.post("/quotes/ajax_new", $(".new_quote").serialize(), function(data) {
+		alert($("form.new_quote").serialize());
+
+		$.post("/quotes/ajax_new", $("form.new_quote").serialize(), function(data) {
 			/*if (data.status == "ok") {
 				$(".msg").text("Ok").fadeIn('slow').delay(1000).fadeOut('slow');
 			}*/
