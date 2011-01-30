fbedd1630f1e041b0b8978392d207575614648b3
shybovycha <shybovycha@gmail.com>
2011-01-30 16:31:39 +0200

    Minor fixes

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,24 +2,19 @@ onerror = function moo(msg, url, line) {
 	//alert('Error: ' + msg + '\nURL: ' + url + '\nLine: ' + line);
 }
 
-/*function moo() {
-	var cnt = 0, type = "";
-	var id = $("div.quote:first").attr("id");
-
-	$.getJSON("/after/" + id, function(data) {
-		var cnt = data.length;
-
-		if (cnt > 0)
-			$(".msg").css("backgrount-color", "#ffff00").text(cnt + " new quotes added. Please, update").fadeIn('slow').delay(20000).fadeOut('slow');
-	});
-
-	setTimeout("moo()", 30000);
-}*/
-
 $(document).ready(function() {
-	//var to = setTimeout("moo()", 30000);
-
 	$.getJSON("/author_list", function(data) { 
 		$("#quote_author").autocomplete({ source: data, minLength: 1 }) 
+	});
+
+	$('.shorten').click(function() {
+		$.getJSON("http://api.bit.ly/v3/shorten", { 
+			"login" : "shybovycha", 
+			"apiKey" : "R_2f8d872eba650d5a459a2d2b7e3e6c5f",
+			"longUrl" : $(this).attr("href"),
+			"format" : "json" }, function(data) {
+				if (data.status_code == 200)
+					$(this).attr("href", data.data.url);
+		})
 	});
 });
